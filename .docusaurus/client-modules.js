61e2d9224a27fa109eaed528afe7b25c331aa325
Joshua Wiedeman <joshua.f.wiedeman@gmail.com>
2024-04-30 20:32:48 -0700

    changes

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\joshu\\Documents\\ShroomDex\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\joshu\\Documents\\ShroomDex\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\joshu\\Documents\\ShroomDex\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\joshu\\Documents\\ShroomDex\\src\\css\\custom.css"),
+];
