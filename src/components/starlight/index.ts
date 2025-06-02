2bc79fefa13db3c68f164adf7323c0b6575b34f8
Joshua Wiedeman <joshua.f.wiedeman@gmail.com>
2025-06-02 11:14:16 -0700

    remove starlight integration

---
+++ src/components/starlight/index.ts
@@ -0,0 +1,9 @@
+export { default as Badge } from './Badge.astro';
+export { default as Card } from './Card.astro';
+export { default as CardGrid } from './CardGrid.astro';
+export { default as Aside } from './Aside.astro';
+export { default as Steps } from './Steps.astro';
+export { default as Tabs } from './Tabs.astro';
+export { default as TabItem } from './TabItem.astro';
+export { default as Icon } from './Icon.astro';
+export { default as LinkButton } from './LinkButton.astro';
