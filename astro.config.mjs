a028bf168e5b15768077c8673dd71e8b64a2b0d8
Joshua Wiedeman <joshua.f.wiedeman@gmail.com>
2025-09-17 15:52:58 -0700

    fix: load starlight config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import starlight from "@astrojs/starlight";
+import starlightConfig from "./starlight.config.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mycosci.com",
-  integrations: [mdx(), starlight()],
+  integrations: [mdx(), starlight(starlightConfig)],
 });
